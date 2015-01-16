49dea7374381568d994afad3a09d6cb3fbad69bc
swilso793 <swilso793@gmail.com>
2015-01-16 10:33:23 +0100

    lots done

--- my_way/populate_rango.py
+++ my_way/populate_rango.py
@@ -10,7 +10,7 @@ from rango.models import Category, Page
 
 
 def populate():
-    python_cat = add_cat('Python')
+    python_cat = add_cat(name='Python')
 
     add_page(cat=python_cat,
         title="Official Python Tutorial",
@@ -57,8 +57,8 @@ def add_page(cat, title, url, views=0):
     p = Page.objects.get_or_create(category=cat, title=title, url=url, views=views)[0]
     return p
 
-def add_cat(name):
-    c = Category.objects.get_or_create(name=name)[0]
+def add_cat(name, views=0, likes=0):
+    c = Category.objects.get_or_create(name=name, views=views, likes=likes)[0]
     return c
 
 # Start execution here!
--- my_way/rango/views.py
+++ my_way/rango/views.py
@@ -1,12 +1,41 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.models import Category
+from tkinter.constants import PAGES
+from rango.forms import CategoryForm
+from rango.models import Category, Page
 
+def add_category(request):
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+        else:
+            print(form.errors)
+    else:
+        form = CategoryForm()
+    return render(request, 'rango/add_category.html', {'form' : form})
+    
 def index(request):
-   # return HttpResponse("Is this an Easter Egg?! <br/> <a href='/rango/about'>About</a>")
-    context_dict = {'boldmessage': 'themessagegoeshere'}
+    #return HttpResponse("Is this an Easter Egg?! <br/> <a href='/rango/about'>About</a>")
+    ##context_dict = {'boldmessage': 'themessagegoeshere'}
+    ##return render(request, 'rango/index.html', context_dict)
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories' : category_list}
     return render(request, 'rango/index.html', context_dict)
 def about(request):
     #return HttpResponse("what is this shit?<br/> <a href='/rango'>Back</a>")
     context_dict = {'italicmessage': 'themessagegoeshere'}
     return render(request, 'rango/about.html', context_dict)
+def category(request, category_name_slug):
+    context_dict = {}
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        context_dict['category_name'] = category.name
+        pages = Page.objects.filter(category=category)
+        context_dict['pages'] = pages
+        context_dict['category'] = Category
+    except Category.DoesNotExist:
+        pass
+    return render(request, 'rango/category.html', context_dict)
