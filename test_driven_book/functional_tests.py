934c5c8f155faf1f601113da291bd24e335257ee
swilso793 <swilso793@gmail.com>
2015-01-23 17:09:39 +0100

    added new test have to alter the template

--- test_driven_book/functional_tests.py
+++ test_driven_book/functional_tests.py
@@ -30,9 +30,8 @@ class NewVisitorTest(unittest.TestCase):
 
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
-		self.assertTrue(
-				any(row.text == '1: Buy peacock feathers' for row in rows)              )
-
+		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+		self.assertIn('2: Buy peacock feathers', [row.text for row in rows])
 		self.fail('Finish the test')
 
 if __name__ == '__main__':
