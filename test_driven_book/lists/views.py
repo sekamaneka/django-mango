ecbc2567250705d3f2e8676b0eb30532a59d6a1a
swilso793 <swilso793@gmail.com>
2015-01-29 23:08:43 +0100

    fixed some things

--- test_driven_book/lists/views.py
+++ test_driven_book/lists/views.py
@@ -19,4 +19,3 @@ def add_item(request, list_id):
     list_ = List.objects.get(id=list_id)
     Item.objects.create(text=request.POST['item_text'], list=list_)
     return redirect('/lists/%d/' % (list_.id,))
-    pass
